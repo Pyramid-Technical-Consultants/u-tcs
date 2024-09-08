1fdde1af8c8bc66a2c12ecd0744391d8536a42d4
Matthew Nichols <mnichols@ptcusa.com>
2024-09-08 17:40:19 -0400

    feat: Added basic DICOM parsing

--- main/dicom/dicomExtractPatient.js
+++ main/dicom/dicomExtractPatient.js
@@ -1,30 +1,26 @@
+import dicomExtractStringFields from "./dicomExtractStringFields"
+
+// https://dicom.innolitics.com/ciods/patient/patient-module
+const fields = {
+  name: "x00100010", // Patient's name
+  nameOther: "x00101001", // Patient's name (alternative representation)
+  id: "x00100020", // Patient ID
+  birthDate: "x00100030", // Patient's birth date
+  birthTime: "x00100032", // Patient's birth time
+  sex: "x00100040", // Patient's sex
+  qualityControlSubject: "x00100200", // Patient's quality control subject
+  speciesDescription: "x00102201", // Patient's species description
+  breedDescription: "x00102292", // Patient's breed description (breed)
+  responsiblePersonName: "x00102297", // Patient's responsible person name
+  responsiblePersonRole: "x00102298", // Patient's responsible person role
+  responsibleOrganization: "x00102299", // Patient's responsible organization
+  comments: "x00104000", // Patient's comments
+  identityRemoved: "x00120062", // Patient's identity removed
+}
+
+// Extracts patient data from a DICOM data set
 function dicomExtractPatient(dataSet) {
-  const patient = {}
-  const fields = {
-    name: "x00100010",
-    nameOther: "x00101001",
-    id: "x00100020",
-    birthDate: "x00100030",
-    birthTime: "x00100032",
-    sex: "x00100040",
-    qualityControlSubject: "x00100200",
-    speciesDescription: "x00102201",
-    breedDescription: "x00102292",
-    responsiblePersonName: "x00102297",
-    responsiblePersonRole: "x00102298",
-    responsibleOrganization: "x00102299",
-    comments: "x00104000",
-    identityRemoved: "x00120062",
-  }
-
-  for (const [key, tag] of Object.entries(fields)) {
-    const value = dataSet.string(tag)
-    if (value !== undefined) {
-      patient[key] = value
-    }
-  }
-
-  return patient
+  return dicomExtractStringFields(dataSet, fields)
 }
 
 export default dicomExtractPatient
